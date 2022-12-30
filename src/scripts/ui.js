ee492d17f5715948d246ce72c5331e841d7e93c3
Ahmed <ahmedmahmoudkzm@gmail.com>
2022-12-30 16:08:22 +0200

    Finished header

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -1,18 +1,28 @@
 import '../styles/style.css';
 import logo from '../assets/images/pictures/logo.jpg';
-import searchLogo from '../assets/images/icons/srch.png'
+import profileLogo from '../assets/images/icons/profile.png';
+import starLogo from '../assets/images/icons/star.png';
+import cartLogo from '../assets/images/icons/cart.png';
 
 const logoImg = new Image();
-const searchImg = new Image();
+const profileImg = new Image();
+const starImg = new Image();
+const cartImg = new Image();
+
 const main = document.getElementById('main-container');
-const header = document.getElementById('header');
+const headerUp = document.getElementById('header-upper');
 const searchContainer = document.getElementById('search-container');
 const actionsContainer = document.getElementById('actions-container');
 
 logoImg.src = logo;
+profileImg.src = profileLogo;
+starImg.src = starLogo;
+cartImg.src = cartLogo;
+
 logoImg.id = 'logo-img';
 
-searchImg.src = searchLogo;
 
-header.prepend(logoImg);
-searchContainer.prepend(searchImg)
+headerUp.prepend(logoImg);
+actionsContainer.append(starImg);
+actionsContainer.append(cartImg);
+actionsContainer.append(profileImg);
