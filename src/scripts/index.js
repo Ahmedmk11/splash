a4c4c0c431b819d4ac2369281861a7d79abbfa4f
Ahmed <ahmedmahmoudkzm@gmail.com>
2022-12-31 02:39:41 +0200

    Added addToFav function and a star symbol to all items

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
+import starLogo from '../assets/images/icons/starB.png';
+import starFilled from '../assets/images/icons/starFilled.png';
+
 const middleContainer = document.getElementById('middle-container');
 
 export function importAll(r) {
@@ -17,17 +20,24 @@ export function populateGrid(imageArr) {
     for (let i = 1; i <= Object.keys(imageArr).length; i++) {
         let tmp = document.createElement("div");
         let info = document.createElement("div");
+        let infoL = document.createElement("div");
         let tmpL = document.createElement("div");
         let nameP = document.createElement("p");
         let priceP = document.createElement("p");
         let hr = document.createElement('hr');
         let img = new Image();
+        let addFav = new Image();
         tmp.classList.add('item');
+        info.classList.add('info');
+        infoL.classList.add('info-left');
         img.src = imageArr[`${i}.jpg`];
+        addFav.src = starLogo;
         nameP.textContent = 'Placeholder Name';
         priceP.textContent = 'EGP 1600';
-        info.append(nameP);
-        info.append(priceP);
+        infoL.append(nameP);
+        infoL.append(priceP);
+        info.append(infoL);
+        info.append(addFav);
         tmp.append(img);
         tmpL.append(hr);
         tmpL.append(info);
@@ -35,4 +45,12 @@ export function populateGrid(imageArr) {
         grid.append(tmp);
     }
     middleContainer.append(grid);
+}
+
+export function addToFav(item) {
+    if (item.src == starFilled) {
+        item.src = starLogo;
+    } else {
+        item.src = starFilled;
+    }
 }
